764a863437f70509e743b999ab1a6458108fa34c
Ralf S. Engelschall <rse@engelschall.com>
2014-09-28 14:23:14 +0200

    add copyright header

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
+/*
+**  slideshow -- Observe and Control Slideshow Applications
+**  Copyright (c) 2014 Ralf S. Engelschall <http://engelschall.com>
+**
+**  This Source Code Form is subject to the terms of the Mozilla Public
+**  License (MPL), version 2.0. If a copy of the MPL was not distributed
+**  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 /* global module: true */
 module.exports = function (grunt) {
