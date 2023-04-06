ca207bd1e97b802d12cd8594500a05b92b5d2995
Ralf S. Engelschall <rse@engelschall.com>
2023-04-06 19:34:48 +0200

    remove all semicolons, they are not needed in modern JavaScript

--- slideshow-api.js
+++ slideshow-api.js
@@ -14,38 +14,38 @@
 /* global require: false */
 /* global module: false */
 
-var connector = require("./connector");
+var connector = require("./connector")
 
 /*  the slideshow API constructor  */
 var slideshow = function (application) {
-    this.connector = new connector(application);
-};
+    this.connector = new connector(application)
+}
 
 /*  the slideshow API methods  */
 slideshow.prototype = {
     request: function (request) {
-        return this.connector.request(request);
+        return this.connector.request(request)
     },
     end: function () {
-        this.connector.end();
+        this.connector.end()
     },
-    "stat":   function ()   { return this.connector.request({ command: "STAT"       }); },
-    "info":   function ()   { return this.connector.request({ command: "INFO"       }); },
-    "boot":   function ()   { return this.connector.request({ command: "BOOT"       }); },
-    "quit":   function ()   { return this.connector.request({ command: "QUIT"       }); },
-    "open":   function (fn) { return this.connector.request({ command: "OPEN " + fn }); },
-    "close":  function ()   { return this.connector.request({ command: "CLOSE"      }); },
-    "start":  function ()   { return this.connector.request({ command: "START"      }); },
-    "stop":   function ()   { return this.connector.request({ command: "STOP"       }); },
-    "pause":  function ()   { return this.connector.request({ command: "PAUSE"      }); },
-    "resume": function ()   { return this.connector.request({ command: "RESUME"     }); },
-    "first":  function ()   { return this.connector.request({ command: "FIRST"      }); },
-    "last":   function ()   { return this.connector.request({ command: "LAST"       }); },
-    "goto":   function (sn) { return this.connector.request({ command: "GOTO " + sn }); },
-    "prev":   function ()   { return this.connector.request({ command: "PREV"       }); },
-    "next":   function ()   { return this.connector.request({ command: "NEXT"       }); }
-};
+    "stat":   function ()   { return this.connector.request({ command: "STAT"       }) },
+    "info":   function ()   { return this.connector.request({ command: "INFO"       }) },
+    "boot":   function ()   { return this.connector.request({ command: "BOOT"       }) },
+    "quit":   function ()   { return this.connector.request({ command: "QUIT"       }) },
+    "open":   function (fn) { return this.connector.request({ command: "OPEN " + fn }) },
+    "close":  function ()   { return this.connector.request({ command: "CLOSE"      }) },
+    "start":  function ()   { return this.connector.request({ command: "START"      }) },
+    "stop":   function ()   { return this.connector.request({ command: "STOP"       }) },
+    "pause":  function ()   { return this.connector.request({ command: "PAUSE"      }) },
+    "resume": function ()   { return this.connector.request({ command: "RESUME"     }) },
+    "first":  function ()   { return this.connector.request({ command: "FIRST"      }) },
+    "last":   function ()   { return this.connector.request({ command: "LAST"       }) },
+    "goto":   function (sn) { return this.connector.request({ command: "GOTO " + sn }) },
+    "prev":   function ()   { return this.connector.request({ command: "PREV"       }) },
+    "next":   function ()   { return this.connector.request({ command: "NEXT"       }) }
+}
 
 /*  export the API  */
-module.exports = slideshow;
+module.exports = slideshow
 
--- slideshow-cli.js
+++ slideshow-cli.js
@@ -17,9 +17,9 @@
 /* global process: false */
 
 /*  external requirements  */
-var readline  = require("readline");
-var chalk     = require("chalk");
-var slideshow = require("./slideshow-api");
+var readline  = require("readline")
+var chalk     = require("chalk")
+var slideshow = require("./slideshow-api")
 
 /*  define the known applications  */
 var apps = {
@@ -30,7 +30,7 @@ var apps = {
     "keynote":        true,
     "keynote5":       true,
     "keynote6":       true
-};
+}
 
 /*  define the known commands (and their argument)  */
 var commands = {
@@ -54,78 +54,78 @@ var commands = {
     "goto":   [ "slide-number" ],
     "prev":   [],
     "next":   []
-};
+}
 
 /*  the interactive CLI variant  */
 var cliInteractive = function () {
     /*  start with a non-existing slideshow  */
-    var ss = null;
+    var ss = null
 
     /*  create the stdin/stdout based readline interface  */
     var rl = readline.createInterface({
         input:  process.stdin,
         output: process.stdout
-    });
+    })
 
     /*  provide CLI  */
-    rl.setPrompt("slideshow> ");
-    rl.prompt();
+    rl.setPrompt("slideshow> ")
+    rl.prompt()
     rl.on("line", function (line) {
         /*  determine command  */
-        var argv = line.trim().split(/\s+/);
-        var info = commands[argv[0]];
-        var prompt = true;
+        var argv = line.trim().split(/\s+/)
+        var info = commands[argv[0]]
+        var prompt = true
         if (typeof info === "undefined")
-            console.log(chalk.red("ERROR: invalid command (use \"help\" for usage)"));
+            console.log(chalk.red("ERROR: invalid command (use \"help\" for usage)"))
         else if (info.length !== (argv.length - 1))
-            console.log(chalk.red("ERROR: invalid number of arguments (expected: " + info.join(" ") + ")"));
+            console.log(chalk.red("ERROR: invalid number of arguments (expected: " + info.join(" ") + ")"))
         else {
             /*  process CLI-specific commands  */
             if (argv[0] === "help")
                 Object.keys(commands).forEach(function (cmd) {
                     console.log(chalk.green("    " + cmd + " " + (
-                        commands[cmd].map(function (arg) { return "<" + arg + ">"; }).join(" ")
-                    )));
-                });
+                        commands[cmd].map(function (arg) { return "<" + arg + ">" }).join(" ")
+                    )))
+                })
             else if (argv[0] === "use") {
                 if (typeof apps[argv[1]] === "undefined")
-                    console.log(chalk.red("ERROR: invalid argument (expected: " + Object.keys(apps).join(", ") + ")"));
+                    console.log(chalk.red("ERROR: invalid argument (expected: " + Object.keys(apps).join(", ") + ")"))
                 else {
                     if (ss !== null)
-                        ss.end();
-                    ss = new slideshow(argv[1]);
-                    rl.setPrompt("slideshow(" + argv[1] + ")> ");
+                        ss.end()
+                    ss = new slideshow(argv[1])
+                    rl.setPrompt("slideshow(" + argv[1] + ")> ")
                 }
             }
 
             /*  process API-derived commands  */
             else {
                 if (ss === null)
-                    console.log(chalk.red("ERROR: you have to choose with \"use\" an application first"));
+                    console.log(chalk.red("ERROR: you have to choose with \"use\" an application first"))
                 else {
                     ss[argv[0]](argv[1]).then(function (response) {
-                        console.log(chalk.green(JSON.stringify(response, null, "    ")));
-                        rl.prompt();
+                        console.log(chalk.green(JSON.stringify(response, null, "    ")))
+                        rl.prompt()
                     }, function (error) {
-                        console.log(chalk.red("ERROR: " + error));
-                        rl.prompt();
-                    });
-                    prompt = false;
+                        console.log(chalk.red("ERROR: " + error))
+                        rl.prompt()
+                    })
+                    prompt = false
                 }
             }
         }
 
         /*  provide prompt for next iteration  */
         if (prompt)
-            rl.prompt();
-    });
+            rl.prompt()
+    })
 
     /*  gracefully stop CLI  */
     rl.on("close", function() {
-        console.log("");
-        process.exit(0);
-    });
-};
+        console.log("")
+        process.exit(0)
+    })
+}
 
 /*  the batch CLI variant  */
 var cliBatch = function (argv) {
@@ -133,42 +133,42 @@ var cliBatch = function (argv) {
         Object.keys(commands).forEach(function (cmd) {
             if (cmd !== "use" && cmd !== "help") {
                 console.log(chalk.green("slideshow <application> " + cmd + " " + (
-                    commands[cmd].map(function (arg) { return "<" + arg + ">"; }).join(" ")
-                )));
+                    commands[cmd].map(function (arg) { return "<" + arg + ">" }).join(" ")
+                )))
             }
-        });
-        process.exit(0);
+        })
+        process.exit(0)
     }
     if (argv.length < 2) {
-        console.log(chalk.red("ERROR: invalid number of arguments (use \"help\" for usage)"));
-        process.exit(1);
+        console.log(chalk.red("ERROR: invalid number of arguments (use \"help\" for usage)"))
+        process.exit(1)
     }
     if (typeof apps[argv[0]] === "undefined") {
-        console.log(chalk.red("ERROR: invalid application (expected: " + Object.keys(apps).join(", ") + ")"));
-        process.exit(1);
+        console.log(chalk.red("ERROR: invalid application (expected: " + Object.keys(apps).join(", ") + ")"))
+        process.exit(1)
     }
-    var ss = new slideshow(argv[0]);
-    var info = commands[argv[1]];
+    var ss = new slideshow(argv[0])
+    var info = commands[argv[1]]
     if (typeof info === "undefined") {
-        console.log(chalk.red("ERROR: invalid connector command (use \"help\" for usage)"));
-        process.exit(1);
+        console.log(chalk.red("ERROR: invalid connector command (use \"help\" for usage)"))
+        process.exit(1)
     }
     else if (info.length !== (argv.length - 2)) {
-        console.log(chalk.red("ERROR: invalid number of connector arguments (expected: " + info.join(" ") + ")"));
-        process.exit(1);
+        console.log(chalk.red("ERROR: invalid number of connector arguments (expected: " + info.join(" ") + ")"))
+        process.exit(1)
     }
     ss[argv[1]](argv[2]).then(function (response) {
-        console.log(chalk.green(JSON.stringify(response, null, "    ")));
-        process.exit(0);
+        console.log(chalk.green(JSON.stringify(response, null, "    ")))
+        process.exit(0)
     }, function (error) {
-        console.log(chalk.red("ERROR: " + error));
-        process.exit(1);
-    });
-};
+        console.log(chalk.red("ERROR: " + error))
+        process.exit(1)
+    })
+}
 
 /*  dispatch according to type of operation  */
 if (process.argv.length === 2)
-    cliInteractive();
+    cliInteractive()
 else
-    cliBatch(process.argv.splice(2));
+    cliBatch(process.argv.splice(2))
 
